a7941e824c0d754b4d51abcb395099c5f8de9ed3
Pablo Romero Pons-Fuster <paromero@c3r3s5.42malaga.com>
2025-06-02 19:23:08 +0200

    empezado cpp02

---
+++ CPP02/ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 18:40:02 by paromero          #+#    #+#             */
+/*   Updated: 2025/06/02 18:40:34 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+
+    c = b;
+    
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    
+    return 0;
+}
