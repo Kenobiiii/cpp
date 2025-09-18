ff55625f627e22846ba62181532a9d3c86a052a2
Pablo Romero Pons-Fuster <paromero@c3r4s2.42malaga.com>
2025-09-18 11:22:41 +0200

    cambiado main al del subject

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,28 +6,21 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 18:40:02 by paromero          #+#    #+#             */
-/*   Updated: 2025/06/04 18:57:18 by paromero         ###   ########.fr       */
+/*   Updated: 2025/09/18 11:14:45 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main( void )
-{
-	const Fixed c;
+int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
-	std::cout << b / c << std::endl;
-
 	std::cout << a << std::endl;
-
 	std::cout << b << std::endl;
-	a = a * Fixed(10000);
 	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
