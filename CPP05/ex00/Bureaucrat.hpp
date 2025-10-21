b200a296301b83dcb5bbb141c4ab463c95a2d110
paromero <pablo.romeroponsfuster@gmail.com>
2025-10-21 18:41:12 +0200

    iniciado cpp05

---
+++ CPP05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 17:32:53 by paromero          #+#    #+#             */
+/*   Updated: 2025/10/21 17:33:55 by paromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Bureaucrat {
+    private:
+        const Name_;
+        int Grade_;
+        
+    public:
+        Bureaucrat();
+        ~Bureaucrat();
+}
