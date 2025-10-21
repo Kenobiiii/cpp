9b83a703651f2836a2036095bf74780caaa7a4eb
paromero <pablo.romeroponsfuster@gmail.com>
2025-10-21 17:04:43 +0200

    Actualizado main

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 12:43:31 by paromero          #+#    #+#             */
-/*   Updated: 2025/10/21 17:02:50 by paromero         ###   ########.fr       */
+/*   Updated: 2025/10/21 17:03:56 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@
 
 int main()
 {
+    Animal* animals[4] = {new Dog(), new Cat(), new Dog(), new Cat()};
+    for (int i = 0; i < 4; i++)
+    {
+        delete animals[i];
+    }
+
     const Animal* j = new Dog();
     const Animal* i = new Cat();
     delete j;//should not create a leak
