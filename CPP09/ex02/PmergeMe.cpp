8528bfed59ca8fa307be0ef41a1082152454fd33
Pablo Romero Pons-Fuster <paromero@c3r6s3.42malaga.com>
2025-11-06 12:32:23 +0100

    empezado cpp09

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -6,9 +6,90 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 20:28:22 by paromero          #+#    #+#             */
-/*   Updated: 2025/11/04 20:28:37 by paromero         ###   ########.fr       */
+/*   Updated: 2025/11/06 09:54:08 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+PmergeMe::PmergeMe(int ac, char **av) {
+    for (int i = 1; i < ac; ++i) {
+        std::stringstream ss(av[i]);
+        int num;
+        if (!(ss >> num) || num < 0) {
+            throw std::runtime_error("Error");
+        }
+        this->dequecont_.push_back(num);
+    }
+}
+
+PmergeMe::~PmergeMe() {
+}
+
+PmergeMe::PmergeMe(const PmergeMe &other) {
+    this->dequecont_ = other.dequecont_;
+}
+
+PmergeMe    PmergeMe::operator=(const PmergeMe &other) {
+    if (this != &other)
+        this->dequecont_ = other.dequecont_;
+    return *this;
+}
+
+void    PmergeMe::sort() {
+    std::deque<std::pair<int, int> > pairs;
+    std::deque<int> main;
+    std::deque<int> pend;
+    int straggler = -1;
+
+    for (size_t i = 0; i < this->dequecont_.size() - 1; i += 2) {
+        int num1 = this->dequecont_[i];
+        int num2 = this->dequecont_[i + 1];
+        if (num1 < num2)
+            pairs.push_back(std::make_pair(num1, num2));
+        else
+            pairs.push_back(std::make_pair(num2, num1));
+    }
+
+    if (this->dequecont_.size() % 2 != 0)
+        straggler = this->dequecont_[this->dequecont_.size() - 1];
+    
+
+    for (size_t j = 0; j < pairs.size(); j++) {
+        main.push_back(pairs[j].second);
+        pend.push_back(pairs[j].first);
+    }
+}
+
+std::deque<int> mergeSort(std::deque<int>& cont) {
+    if (cont.size() <= 1)
+        return cont;
+    
+    size_t mid = cont.size() / 2;
+    std::deque<int> left(cont.begin(), cont.begin() + mid);
+    std::deque<int> right(cont.begin() + mid, cont.end());
+    
+    left = mergeSort(left);
+    right = mergeSort(right);
+    
+    return merge(left, right);
+}
+
+std::deque<int> merge(std::deque<int>& left, std::deque<int>& right) {
+    std::deque<int> result;
+    size_t i = 0, j = 0;
+    
+    while (i < left.size() && j < right.size()) {
+        if (left[i] <= right[j])
+            result.push_back(left[i++]);
+        else
+            result.push_back(right[j++]);
+    }
+    
+    while (i < left.size())
+        result.push_back(left[i++]);
+    while (j < right.size())
+        result.push_back(right[j++]);
+    
+    return result;
+}
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 20:26:26 by paromero          #+#    #+#             */
-/*   Updated: 2025/11/04 20:28:05 by paromero         ###   ########.fr       */
+/*   Updated: 2025/11/06 09:50:29 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,21 @@
 #include <iostream>
 #include <climits>
 #include <sstream>
+#include <algorithm>
+#include <utility>
 
 class PmergeMe {
     private:
         std::deque<int> dequecont_;
     
     public:
-        PmergeMe();
+        PmergeMe(int ac, char **av);
         ~PmergeMe();
         PmergeMe(const PmergeMe &other);
         PmergeMe operator=(const PmergeMe &other);
+
+        void    sort();
 };
+
+std::deque<int> mergeSort(std::deque<int>& cont);
+std::deque<int> merge(std::deque<int>& left, std::deque<int>& right);
